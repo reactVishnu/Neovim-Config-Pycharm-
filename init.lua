3d53194d7094c0f6eed549eebc3afa8d0e3ab560
reactVishnu <thirdparties47@gmail.com>
2024-02-21 11:59:08 +0530

    init.lua python execution added

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
@@ -6,4 +6,5 @@
 --   command = "tabdo wincmd =",
 -- })
 --
-
+vim.api.nvim_set_keymap('n', '<F9>', ':w<CR>:split term://python %<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<F9>', '<Esc>:w<CR>:split term://python %<CR>', { noremap = true, silent = true })
