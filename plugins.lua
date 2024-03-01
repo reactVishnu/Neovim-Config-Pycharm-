eb594d6b5dba26a1298c1e793d7cb324d36ade2d
reactVishnu <thirdparties47@gmail.com>
2024-03-01 22:19:28 +0530

    Harpoon Added

--- plugins.lua
+++ plugins.lua
@@ -70,7 +70,8 @@ local plugins = {
   { import = "custom.configs.vishnu.autosession" },
   { import = "custom.configs.vishnu.pdf-live" },
   { import = "custom.configs.vishnu.md-preview"},
-  { import = "custom.configs.vishnu.codewindow" }
+  { import = "custom.configs.vishnu.codewindow" },
+  { import = "custom.configs.vishnu.harpoon"}
   -- { import = "custom.configs.vishnu.coc-plugin"},
     -- To make a plugin not be loaded
   -- {
